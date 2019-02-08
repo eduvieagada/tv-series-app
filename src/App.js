db64fcfa75d13d1a621f7032a393eee36a8f0854
Nonso Chukwuogor <nonsochukwuogor@gmail.com>
2019-02-08 16:42:36 +0100

    modified the home page and added new component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const Intro = (props) => (
+  <p className="App-intro">
+    Our first functional component
+  </p>
+);
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Intro/>
       </div>
     );
   }
